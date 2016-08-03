47ebfddf17cfb6360e729d5fb77dc413abc9308d
Elvira Khabirova <lineprinter0@gmail.com>
2016-08-03 19:46:29 +0300

    SYS_FUNC(getrandom): structured-output-ify.

--- getrandom.c
+++ getrandom.c
@@ -5,11 +5,11 @@ SYS_FUNC(getrandom)
 {
 	if (exiting(tcp)) {
 		if (syserror(tcp))
-			printaddr(tcp->u_arg[0]);
+			s_push_addr(tcp->u_arg[0]);
 		else
-			printstr(tcp, tcp->u_arg[0], tcp->u_rval);
-		tprintf(", %lu, ", tcp->u_arg[1]);
-		printflags(getrandom_flags, tcp->u_arg[2], "GRND_???");
+			s_push_str(tcp->u_arg[0], tcp->u_rval);
+		s_push_int_lu(tcp->u_arg[1]);
+		s_push_flags_int(getrandom_flags, tcp->u_arg[2], "GRND_???");
 	}
 	return 0;
 }
