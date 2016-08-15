63efe6065f87b2de791b7510e566e6163fca900c
Elvira Khabirova <lineprinter0@gmail.com>
2016-08-16 01:55:02 +0300

    Update fchownat parser with regard to new id type definitions

--- fchownat.c
+++ fchownat.c
@@ -4,8 +4,8 @@ SYS_FUNC(fchownat)
 {
 	s_push_dirfd("dirfd");
 	s_push_path("pathname");
-	s_push_uid("owner");
-	s_push_gid("group");
+	s_push_uid_1("owner");
+	s_push_gid_1("group");
 	s_push_flags_int("flags", at_flags, "AT_???");
 
 	return RVAL_DECODED;
