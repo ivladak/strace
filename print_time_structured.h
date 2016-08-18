d631ef6649896e6fbf3ccc1e0aaf7b4535b17660
Elvira Khabirova <lineprinter0@gmail.com>
2016-08-18 04:43:15 +0300

    Add declarations of s_push_itimerval*

--- print_time_structured.h
+++ print_time_structured.h
@@ -6,4 +6,9 @@ void s_push_timespec(const char *name);
 void s_insert_timeval_addr(const char *name, long addr);
 void s_push_timeval(const char *name);
 
+void s_push_itimerval(const char *name);
+#ifdef ALPHA
+void s_push_itimerval32(const char *name);
+#endif /* ALPHA */
+
 #endif /* !defined(STRACE_PRINT_TIME_H) */
