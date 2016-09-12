02e8bc663e773935de6cf926e0683056b4436573
Elvira Khabirova <lineprinter0@gmail.com>
2016-09-12 03:13:11 +0300

    futex: Add comments to parameters

--- futex.c
+++ futex.c
@@ -52,6 +52,8 @@ SYS_FUNC(futex)
 
 	const unsigned int val3 = tcp->u_arg[5];
 
+	s_set_comment_level(S_SAN_PREFERRED);
+
 	s_push_addr("uaddr");
 	s_push_xlat_int("futex_op", futexops, "FUTEX_???");
 
@@ -67,45 +69,60 @@ SYS_FUNC(futex)
 		s_push_u("val");
 		s_push_timespec("timeout");
 		s_insert_x("val3", val3);
+		s_append_comment("bitset");
 		break;
 	case FUTEX_WAKE_BITSET:
 		s_push_u("val");
 		s_insert_x("val3", val3);
+		s_append_comment("bitset");
 		break;
 	case FUTEX_REQUEUE:
 		s_push_u("val");
+		s_append_comment("processes to wake");
 		s_push_u("val2");
+		s_append_comment("processes to requeue");
 		s_push_addr("uaddr2");
+		s_append_comment("futex to reqeueue to");
 		break;
 	case FUTEX_CMP_REQUEUE:
 	case FUTEX_CMP_REQUEUE_PI:
 		s_push_u("val");
+		s_append_comment("processes to wake");
 		s_push_u("val2");
+		s_append_comment("processes to requeue");
 		s_push_addr("uaddr2");
+		s_append_comment("futex to reqeueue to");
 		s_push_u("val3");
+		s_append_comment("value of requeue target");
 		break;
 	case FUTEX_WAKE_OP:
 		s_push_u("val");
 		s_push_u("val2");
+		s_append_comment("processes to wake on uaddr2 if op is successful");
 		s_push_addr("uaddr2");
+		s_append_comment("futex to apply operation upon");
+
+		s_insert_xlat_int_empty("val3");
 
 		/* In order to avoid generation of leading "0|" */
 		if (val3 & (8 << 28))
-			s_insert_xlat_int_scaled("op", NULL, val3 & (8 << 28),
-				"FUTEX_OP_OPARG_SHIFT", 28);
+			s_append_xlat_int_ex("op", NULL, val3 & (8 << 28),
+				"FUTEX_OP_OPARG_SHIFT", 28,
+				"oparg should be treated as power of 2");
 		s_append_xlat_int_ex("op", futexwakeops,
-			val3 &   (0x7 << 28), "FUTEX_OP_???", 28, NULL);
+			val3 &   (0x7 << 28), "FUTEX_OP_???", 28, "op");
 		s_append_xlat_int_ex("oparg", NULL,
-			val3 & (0xfff << 12), NULL, 12, NULL);
+			val3 & (0xfff << 12), NULL, 12, "oparg");
 		s_append_xlat_int_ex("cmp", futexwakecmps,
-			val3 &   (0xf << 24), "FUTEX_OP_CMP_???", 24, NULL);
+			val3 &   (0xf << 24), "FUTEX_OP_CMP_???", 24, "cmp");
 		s_append_xlat_int_ex("cmparg", NULL,
-			val3 & (0xfff <<  0), NULL, 0, NULL);
+			val3 & (0xfff <<  0), NULL, 0, "cmparg");
 		break;
 	case FUTEX_WAIT_REQUEUE_PI:
 		s_push_u("val");
 		s_push_timespec("timeout");
 		s_insert_addr("uaddr2", uaddr2);
+		s_append_comment("PI futex to requeue processes on");
 		break;
 	case FUTEX_FD:
 	case FUTEX_WAKE:
